8a944145610c5c193fed1a58330f4372779534a8
AgnieszkaM <118823181+AgnieszkaBioinformatics@users.noreply.github.com>
2025-01-08 12:31:42 +0100

    iqtree d ukorzenione

---
+++ workflow/d_ukorzenione.sh
@@ -0,0 +1,10 @@
+DIR=""
+INPUTS="${DIR}/msa_fasta"
+OUTPUTS="${DIR}/fast_trees"
+
+for file in ${INPUTS}/*fasta
+do
+        echo $file
+        NAME=$(basename "$file" .fasta)
+        ./FastTree < "$file" > "${OUTPUTS}/${NAME}.nwk"
+done
